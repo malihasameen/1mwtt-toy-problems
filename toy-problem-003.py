3078ecaccecb6ebd6855497b60dc20ff9e40a714
Maliha Sameen <malihasameen58@gmail.com>
2018-07-10 06:01:17 +0500

    added solution with built-in function

--- toy-problem-003.py
+++ toy-problem-003.py
@@ -1,10 +1,29 @@
 """
+  * http://www.pythonchallenge.com/pc/def/0.html
+  * Python Challenge Level 0
+"""
+# power operator
+print (2**38)
+
+# power function
+print(pow(2,38))
+
+# loop
+n = 1
+for i in range(38):
+    n *= 2
+print(n)
+
+# shift
+print(1 << 38)
+
+"""
   * http://www.pythonchallenge.com/pc/def/map.html
   * Python Challenge Level 1
 """
 raw = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 
-# Beginner Solution
+# beginner
 print("Beginner Solution")
 result = ""
 for c in raw:
@@ -14,13 +33,13 @@ for c in raw:
         result += c
 print(result)
 
-# Expert Solution
-print("Expert Solution")
+# advanced
+print("Advance Solution")
 result = "".join([chr(((ord(c)+2) - ord('a')) % 26 + ord('a')) if c.isalpha() else c for c in raw])
 print(result)
 
-# Solution with built-in function
+# solution with built-in function
 print("Solution with Built-in Function")
 table = str.maketrans("abcdefghijklmnopqrstuvwxyz","cdefghijklmnopqrstuvwxyzab")
-raw.translate(table)
-print(result)
+print(raw.translate(table))
+
